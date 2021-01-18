e91e2d43a8925dd5ad42fbc018ea1ffd9afcced3
Ned <no13beki@studserv.uni-leipzig.de>
2021-01-18 16:54:34 +0100

    testing rightclick on hold bug on phone

--- public/game.js
+++ public/game.js
@@ -138,7 +138,7 @@ class Game{
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 		this.renderer.shadowMap.enabled = true;
 		this.container.appendChild( this.renderer.domElement );
-		
+
 		this.graffitiButton = document.createElement('button');
 		this.graffitiButton.id = 'grafButton'
 		this.graffitiButton.style.z = '0';
@@ -154,7 +154,7 @@ class Game{
 		this.graffitiButton.style.background = 'rgba(126, 126, 126, 0.5)';
 		this.graffitiButton.style.userDrag = 'none';
 		this.graffitiButton.style.userSelect = 'none';
-		this.graffitiButton.style.pointerEvents = 'none';
+
 
 		this.sprayButton = document.createElement('button');
 		this.sprayButton.id = 'grafButton'
@@ -170,7 +170,6 @@ class Game{
 		this.sprayButton.style.background = 'rgba(126, 126, 126, 0.5)';
 		this.sprayButton.style.userDrag = 'none';
 		this.sprayButton.style.userSelect = 'none';
-		this.sprayButton.style.pointerEvents = 'none';
 
 
 		this.graffitiIcon = document.createElement('img');
@@ -183,7 +182,6 @@ class Game{
 		this.graffitiIcon.style.userDrag = 'none';
 		this.graffitiIcon.style.userSelect = 'none';
 		this.graffitiIcon.draggable = false;
-		this.graffitiIcon.style.pointerEvents = 'none';
 
 		this.runIcon = document.createElement('img');
 		this.runIcon.style.width = "50px";
@@ -194,8 +192,48 @@ class Game{
 		this.runIcon.style.userDrag = 'none';
 		this.runIcon.style.userSelect = 'none';
 		this.runIcon.draggable = false;
-		this.runIcon.style.pointerEvents = 'none';
 
+		// Simple example, see optional options for more configuration.
+		// const pickr = Pickr.create({
+		// 	el: '.color-picker',
+		// 	theme: 'nano', // or 'monolith', or 'nano'
+		// 	swatches: [
+		// 		'rgba(244, 67, 54, 1)',
+		// 		'rgba(233, 30, 99, 0.95)',
+		// 		'rgba(156, 39, 176, 0.9)',
+		// 		'rgba(103, 58, 183, 0.85)',
+		// 		'rgba(63, 81, 181, 0.8)',
+		// 		'rgba(33, 150, 243, 0.75)',
+		// 		'rgba(3, 169, 244, 0.7)',
+		// 		'rgba(0, 188, 212, 0.7)',
+		// 		'rgba(0, 150, 136, 0.75)',
+		// 		'rgba(76, 175, 80, 0.8)',
+		// 		'rgba(139, 195, 74, 0.85)',
+		// 		'rgba(205, 220, 57, 0.9)',
+		// 		'rgba(255, 235, 59, 0.95)',
+		// 		'rgba(255, 193, 7, 1)'
+		// 	],
+
+		// 	components: {
+
+		// 		// Main components
+		// 		preview: true,
+		// 		opacity: false,
+		// 		hue: true,
+
+		// 		// Input / output Options
+		// 		interaction: {
+		// 			hex: true,
+		// 			rgba: true,
+		// 			hsla: true,
+		// 			hsva: true,
+		// 			cmyk: true,
+		// 			input: true,
+		// 			clear: true,
+		// 			save: true
+		// 		}
+		// 	}
+		// });
 		
 		document.body.appendChild(game.graffitiButton);
 		this.graffitiButton.appendChild(game.graffitiIcon);
